ea7a4ee22c4272bc8d35754e550a39fb403adb65
nightfriday62 <sunday-night@outlook.com>
2024-12-04 17:18:09 -0800

    Create a new driver_local.py file that is the same as current driver.py functionally, but load map information and airbnb nodes from file to save running time. The utility that creates local files is also uploaded as well as the local files.

---
+++ local_storage_utility.py
@@ -0,0 +1,16 @@
+import osmnx as ox
+import data_loader as dl
+import json as js
+# Load and Save the road network of Manhattan
+ROAD_NETWORK = ox.graph_from_place("Manhattan, New York, USA", network_type="drive")
+ox.save_graphml(ROAD_NETWORK, filepath="manhattan_road_network.graphml")
+
+# Load Airbnb data and save the nodes
+AIRBNB_FILE_PATH = 'new_york_airbnb_2024.csv'
+airbnb_data = dl.load_airbnb_data(AIRBNB_FILE_PATH)
+
+airbnb_nodes = [
+        ox.distance.nearest_nodes(ROAD_NETWORK, row['longitude'], row['latitude']) for _, row in airbnb_data.iterrows()
+    ]
+
+js.dump(airbnb_nodes, open("airbnb_nodes.json", "w"))
